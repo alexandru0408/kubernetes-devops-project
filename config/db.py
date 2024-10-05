f68776bd2ced27cf5337f88b998d854018afba60
alexandru0408 <poznan.alin@gmail.com>
2024-10-05 14:54:23 +0000

    fix db url

--- config/db.py
+++ config/db.py
@@ -1,7 +1,12 @@
 import os
 from sqlalchemy import create_engine, MetaData
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DB_USERNAME = os.getenv("DB_USERNAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+DB_HOST = os.getenv("DB_HOST", "db")  # Default to 'db' if not set
+
+DATABASE_URL = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}"
 engine = create_engine(DATABASE_URL)
 
 meta = MetaData()
